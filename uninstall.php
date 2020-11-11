4eb22d2a4c30c3b28f749c3c48e4f217796d472f
webmarcello8080 <webmarcello8080@gmail.com>
2020-11-11 16:42:09 +0000

    uninstall options

--- uninstall.php
+++ uninstall.php
@@ -6,6 +6,7 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
 
 delete_option( 'wp-clickable-background-active' );
 delete_option( 'wp-clickable-background-link' );
+delete_option( 'wp-clickable-background-class' );
 delete_option( 'wp-clickable-background-new' );
 delete_option( 'wp-clickable-background-javascript' );
 delete_option( 'wp-clickable-background-form-message');
