e0c1dffd7eefd212d311848579fcda90861d2830
webmarcello8080 <webmarcello8080@gmail.com>
2021-02-20 12:32:40 +0000

    merging

--- wp-clickable-background.php
+++ wp-clickable-background.php
@@ -3,15 +3,12 @@
  * Plugin Name:       WP Clickable Background
  * Plugin URI:        https://github.com/webmarcello8080/wp-clickable-background
  * Description:       Wordpress Plugin in order to make the background of your website clickable.
- * Version:           1.0.0
+ * Version:           1.0.1
  * Requires at least: 4.5.13
  * Requires PHP:      5.6
  * Author:            Marcello Perri
  * Author URI:        http://webmarcello.co.uk
  */
-/*
-- article on website
-*/
  define('PLUGIN_WCB_BASENAME', plugin_basename(__FILE__) );
 
 foreach ( glob( plugin_dir_path( __FILE__ ) .'includes/*.php') as $filename){
